a9890deb3d147359b103f0b56e8ff18960d4f074
Ankubana <kubalpha19@gmail.com>
2025-06-20 03:34:14 -0700

    Add files via upload

---
+++ selectedMove.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { Link,useNavigate} from "react-router-dom";
+import MovieDesc from "./Movie_description";
+import axios from "axios";
+ const SelectMovie=({selectMovie,inputval})=>{
+     const [movieId,setMovieId]=useState("")
+     const [data,setData]=useState()
+      const Navigate = useNavigate();
+   const filteredMovies = selectMovie?.Search?.filter(
+    (movie) => movie?.Poster && movie.Poster !==""
+  ) || [];
+     console.log(filteredMovies)
+   async  function movie_Description(value){
+     const MovieId=value
+
+     const{data}=await axios.get(`https://omdbapi.com/?apikey=4cfe7eb4&s=${value}`)
+          setData(data)
+          setMovieId(MovieId)
+      
+          if (MovieId)
+           Navigate(`/Movie_description/${MovieId}`);
+    }
+    
+return(
+<>
+      
+     {
+
+  
+      filteredMovies?.slice(0,6).map((movie)=>(
+         <> <div>
+            <div className="movie_card">
+  <img 
+    src={movie?.Poster} 
+    alt={movie?.Title} 
+    className="project__images zoom-image"  
+    onClick={() => movie_Description(movie.imdbID)} 
+  />
+
+  <div className="movie_Desc">
+    <span className="movie_Title">Title: </span>{movie?.Title}
+  </div>
+  <div className="movie_Desc">
+    <span className="movie_Year">Year: </span>{movie?.Year}
+  </div>
+  <div className="movie_Desc">
+    <span className="movie_Type">Type: </span>{movie?.Type}
+  </div>
+</div>
+          { movieId?
+          <MovieDesc  Data={data}  idmb={movieId}/>
+           :null
+ } 
+    </div>
+   
+            </>
+    
+      )
+       
+        )
+      
+   
+      }
+</>
+
+  )
+
+ }
+ export default SelectMovie
