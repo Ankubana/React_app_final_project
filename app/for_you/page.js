e081e03b096dcc7f2963e89ff9635db8ebb2a026
Alphonse <kubalpha19@gmail.com>
2026-01-09 00:51:27 -0800

    update for you paage

--- app/for_you/page.js
+++ app/for_you/page.js
@@ -5,7 +5,13 @@ import Link from "next/link";
 import { HomeIcon, BookmarkIcon,ArrowRightOnRectangleIcon,
   PencilIcon,QuestionMarkCircleIcon,MagnifyingGlassIcon,  Cog6ToothIcon } from '@heroicons/react/24/outline';
 import Nav from "../components/nav";
+import { useState } from "react";
 export default function Landing() {
+const [activeTab, setActiveTab] = useState("");
+   const forYou=()=>{
+ 
+      setActiveTab("/for-you")
+   }
   return (
    <div id="__next">
     <div className={for_you_styles.wrapper}>
@@ -34,14 +40,15 @@ export default function Landing() {
     <div className={for_you_styles.sidebar__wrapper}>
     <div className={for_you_styles.sidebar__top}>
     <a className={for_you_styles.sidebar__link__wrapper} href="/for-you">
-    <div className={`${for_you_styles.sidebar__link_line} ${for_you_styles.active__tab}`}>
-    </div>
+    <div className={`${for_you_styles.sidebar__link_line} ${activeTab === "/for-you" ? for_you_styles.active__tab : ""}`}onClick={forYou}>
+    </div>                                                                                                                                  
     <div className={for_you_styles.sidebar__icon__wrapper}>
       <HomeIcon/>
     </div>
     <div className={for_you_styles.sidebar__link__text}>For you</div>
     </a><a className={for_you_styles.sidebar__link__wrapper} href="/library">
-    <div className={for_you_styles.sidebar__link_line}>
+     <div className={`${for_you_styles.sidebar__link_line} ${
+      activeTab === "/for-you" ? for_you_styles.active__tab : ""}`}>
     </div>
     <div className={for_you_styles.sidebar__icon__wrapper}>
      <BookmarkIcon/>
@@ -49,32 +56,32 @@ export default function Landing() {
     <div className={for_you_styles.sidebar__link__text}>My Library</div>
     </a>
     <div className={`${for_you_styles.sidebar__link__wrapper} ${for_you_styles.sidebar__link__not_allowed}`}>
-    <div className={for_you_styles.sidebar__link_line}>
+     <div className={`${for_you_styles.sidebar__link_line} ${for_you_styles.active__tab}`}>
     </div><div className={for_you_styles.sidebar__icon__wrapper}>
     <PencilIcon/>
     </div>
     <div className={for_you_styles.sidebar__link__text}>Highlights</div></div>
     <div className={`${for_you_styles.sidebar__link__wrapper} ${for_you_styles.sidebar__link__not_allowed}`}>
-    <div className={for_you_styles.sidebar__link_line}>
+     <div className={`${for_you_styles.sidebar__link_line} ${for_you_styles.active__tab}`}>
     </div><div className={for_you_styles.sidebar__icon__wrapper}>
      <MagnifyingGlassIcon/>
     </div>
     <div className={for_you_styles.sidebar__link__text}>Search</div>
     </div></div><div className={for_you_styles.for_you_stylessidebar__bottom}>
     <a className={for_you_styles.sidebar__link__wrapper} href="/settings">
-    <div className={for_you_styles.sidebar__link_line}></div>
+     <div className={`${for_you_styles.sidebar__link_line} ${for_you_styles.active__tab}`}/>
     <div className={for_you_styles.sidebar__icon__wrapper}>
         < Cog6ToothIcon/>
     </div><div className={for_you_styles.sidebar__link__text}>Settings</div>
       </a><div className={`${for_you_styles.sidebar__link__wrapper} ${for_you_styles.sidebar__link__not_allowed}`}>
-      <div className={for_you_styles.sidebar__link_line}>
+       <div className={`${for_you_styles.sidebar__link_line} ${for_you_styles.active__tab}`}>
       </div><div className={for_you_styles.sidebar__icon__wrapper}>
       < QuestionMarkCircleIcon/>
       </div>
       <div className={for_you_styles.sidebar__link__text}>Help &amp; Support</div>
       </div>
       <div className={for_you_styles.sidebar__link__wrapper}>
-      <div className={for_you_styles.sidebar__link__line}></div>
+       <div className={`${for_you_styles.sidebar__link_line} ${for_you_styles.active__tab}`}/>
       <div className={for_you_styles.sidebar__icon__wrapper}>
       < ArrowRightOnRectangleIcon/>
       </div>
